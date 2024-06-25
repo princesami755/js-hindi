a29462a6233b54db1a0eaab2293b9576d2c22dc9
princesami755 <anusraja124@gmail.com>
2024-06-25 09:45:54 +0000

    varieble

---
+++ 01_basics/01_varieble.js
@@ -0,0 +1,16 @@
+const accountId = 421016543
+var accountEmail = "anusraja124@gmail.com"
+let accountPassword = "4211013"
+accountcity = "larkana"
+
+
+// accountId = 7 // not allowed
+
+accountEmail = "sami@gmail.com"
+accountPassword = "32156912"
+accountcity = "karachi"
+
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity])
