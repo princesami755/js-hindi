72b32caac955a228e24a5090bfacc1d601d11050
princesami755 <anusraja124@gmail.com>
2024-06-26 09:08:56 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version //
+
+
+// alert( 3 + 3) we are using Node.js, Not browser
+
+
+let name = "sami"
+let age = 17
+let inloggedin = false
+ 
+   // PRIMITIVE DATA TYPES  
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// nul => standalone value
+// undefind =>
+// symbol => unique 
+
+ // OBJECT 
+  
+ console.log(typeof undefind); // undefind
+ console.log(typeof null);    // null
+
+
