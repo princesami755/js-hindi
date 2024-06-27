dec2228576bb2870bb9a00b2e6d927e29d990ef7
princesami755 <anusraja124@gmail.com>
2024-06-27 06:35:04 +0000

    conversion operation

---
+++ 01_basics/03_conversionoperate.js
@@ -0,0 +1,30 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+ 
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN    =>    (Not An Number)
+// true => 1;  false => 0;
+
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+
+console.log(typeof (stringNumber))
