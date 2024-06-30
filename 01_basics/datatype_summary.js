a0aae7418ad309abfc40a3d1bb6f672a772a7ed0
princesami755 <anusraja124@gmail.com>
2024-06-30 07:44:06 +0000

    summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,43 @@
+// PRIMITIVE DATA TYPES
+
+// 7 type : string, number, boolean, null, undefined, symbol, BigInt
+
+
+const score = 100 
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherid = symbol('123')
+
+// console.log(id === anotherid)
+
+const bignumber = 4618321482643912041n
+
+
+// REFERENCE (NON PRIMITIVE)
+
+// array, object, function
+
+
+// ARRAY
+
+const heros = [ferozkhan, danishtaimoor, fahadmustafa]
+
+// OBJECT
+
+let myobj = {
+
+    name: "sami",
+    age: 17,
+}
+
+// FUNCTION
+
+let myfunction = function(){
+    console.log("hello world");
+
+}
